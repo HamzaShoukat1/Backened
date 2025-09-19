c0c882e74fa7bbd9a088319038fff8c3df2fd3dc
hamza <hamza123ali45678@gmail.com>
2025-09-19 23:11:18 +0500

    connect to mongo atlas

---
+++ src/db/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDb = async()=>{
+  try {
+    const connectioninstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    console.log(`\n mongodb connected,DB host: ${connectioninstance.connection.host}`)
+    
+  } catch (error) {
+    console.log("mongo connection error",error)
+    process.exit(1)
+    
+  }
+
+}
+export default connectDb
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import dotenv from "dotenv"
+import connectDb from "./db/db.js"
+
+
+dotenv.config({
+  path: './public/temp/.env'
+})
+
+
+connectDb()
